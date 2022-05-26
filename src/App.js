182daaec0bf313ff54dadb0ad57e8150926f6bd4
abhijeet <abhijeetsinha43@gmail.com>
2022-05-26 20:17:15 +0530

    feat: added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LandingPage, Home, Explore, Bookmark, Profile } from "./Pages/";
 function App() {
-  return <Profile />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/bookmark" element={<Bookmark />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { AiOutlineHome, AiOutlinePlusCircle } from "react-icons/ai";
 import { MdOutlineExplore } from "react-icons/md";
 import { BsBookmark } from "react-icons/bs";
+import { NavLink, useNavigate } from "react-router-dom";
 import userImg from "../../assets/userOne.jpg";
 import "./Header.css";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <header className="header">
       <navbar className="navbar">
@@ -16,21 +19,31 @@ const Header = () => {
         </div>
         <ul className="nav-items">
           <li className="nav-item">
-            <AiOutlineHome title="Home" />
+            <NavLink to="/home">
+              <AiOutlineHome title="Home" />
+            </NavLink>
           </li>
           <li className="nav-item">
-            <MdOutlineExplore title="Explore" />
-          </li>
-          <li className="nav-item active">
-            <BsBookmark title="Bookmark" />
+            <NavLink to="/explore">
+              <MdOutlineExplore title="Explore" />
+            </NavLink>
           </li>
           <li className="nav-item">
-            <AiOutlinePlusCircle title="Create Post" />
+            <NavLink to="/bookmark">
+              <BsBookmark title="Bookmark" />
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <button>
+              <AiOutlinePlusCircle title="Create Post" />
+            </button>
           </li>
           <li className="nav-item" title="Profile">
-            <div className="user-avatar">
-              <img src={userImg} alt="user-profile" />
-            </div>
+            <button onClick={() => navigate("/profile")}>
+              <div className="user-avatar">
+                <img src={userImg} alt="user-profile" />
+              </div>
+            </button>
           </li>
         </ul>
       </navbar>
