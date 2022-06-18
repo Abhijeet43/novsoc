066443b3a0ffa7e3cd7587cfb628541e57b564aa
abhijeet <abhijeetsinha43@gmail.com>
2022-06-18 10:53:52 +0530

    feat: added validations to signup

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
 export { sortByDate } from "./sortByDate";
 export { sortByTrending } from "./sortByTrending";
 export { searchHandler } from "./searchHandler";
+export { validatePassword } from "./validatePassword";
+export { confirmPasswordCheck } from "./confirmPasswordCheck";
